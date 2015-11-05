1e0a6d0f21e494543b8ca90da523a8545e105848
mizutaki <mizutaki4@gmail.com>
2015-11-05 23:24:39 +0900

    be able to download a file from the browser

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@ require 'sinatra/base'
 require 'sinatra/reloader'
 require 'prawn'
 require 'prawn/table'
+require 'pp'
 
 class MainApp < Sinatra::Base
   set :public, File.dirname(__FILE__) + '/public'
@@ -11,13 +12,20 @@ class MainApp < Sinatra::Base
     erb :index
   end
 
-  post '/output_pdf' do
-    time = params['time'].to_i
-    timetable = Array.new(time) { Array.new(5) }
-    time.times do |t|
-      timetable[t][0] = t +1
-    end
-    f_name = File.basename(__FILE__,".rb")+".pdf"
+  get '/output_pdf' do
+    timetable = Array.new(6) { Array.new(6) }
+    timetable[0][1] = "Mon"
+    timetable[0][2] = "Tues"
+    timetable[0][3] = "Wed"
+    timetable[0][4] = "Thur"
+    timetable[0][5] = "Fri"
+    timetable[1][0] = 1
+    timetable[2][0] = 2
+    timetable[3][0] = 3
+    timetable[4][0] = 4
+    timetable[5][0] = 5
+    file_name = File.basename(__FILE__,".rb")+".pdf"
+    f_name = "public/images/#{file_name}"
     Prawn::Document.generate(f_name) {
       stroke_axis
   
@@ -26,5 +34,7 @@ class MainApp < Sinatra::Base
         table timetable
       }
     }
+    response.headers["Content-Disposition"] = "attachment"
+    send_file f_name
   end
 end
