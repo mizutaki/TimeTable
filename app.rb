060a8c7d36e72ab006470d1da3d4e67c0e4c1dbe
mizutaki <mizutaki4@gmail.com>
2015-11-07 23:22:23 +0900

    refactoring

--- app.rb
+++ app.rb
@@ -3,7 +3,6 @@ require 'sinatra/base'
 require 'sinatra/reloader'
 require 'prawn'
 require 'prawn/table'
-require 'pp'
 
 class MainApp < Sinatra::Base
   set :public, File.dirname(__FILE__) + '/public'
@@ -13,17 +12,16 @@ class MainApp < Sinatra::Base
   end
 
   get '/output_pdf' do
-    timetable = Array.new(6) { Array.new(6) }
-    timetable[0][1] = "Mon"
-    timetable[0][2] = "Tues"
-    timetable[0][3] = "Wed"
-    timetable[0][4] = "Thur"
-    timetable[0][5] = "Fri"
-    timetable[1][0] = 1
-    timetable[2][0] = 2
-    timetable[3][0] = 3
-    timetable[4][0] = 4
-    timetable[5][0] = 5
+    period = params[:period].to_i
+    timetable = []
+    timetable_dayofweek = ["","Mon","Tues","Wed","Thur","Fri"]
+    timetable << timetable_dayofweek
+    period.times do |pt|
+      pt+=1
+      timeTable_record = Array.new(timetable_dayofweek.length)
+      timeTable_record[0] = pt
+      timetable << timeTable_record
+    end
     file_name = File.basename(__FILE__,".rb")+".pdf"
     f_name = "public/images/#{file_name}"
     Prawn::Document.generate(f_name) {
