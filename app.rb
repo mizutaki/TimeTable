fce3c8a2c8dec3f6f45122f99760513badee6fc5
mizutaki <mizutaki4@gmail.com>
2015-11-08 16:15:29 +0900

    change it to make PDF from the input item

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@ require 'sinatra/base'
 require 'sinatra/reloader'
 require 'prawn'
 require 'prawn/table'
+require 'json'
 
 class MainApp < Sinatra::Base
   set :public, File.dirname(__FILE__) + '/public'
@@ -11,20 +12,26 @@ class MainApp < Sinatra::Base
     erb :index
   end
 
-  get '/output_pdf' do
-    period = params[:period].to_i
+  get '/download_pdf/:file_name' do
+    response.headers["Content-Disposition"] = "attachment"
+    file_name = params['file_name']
+    file_path = "public/images/#{file_name}"
+    send_file file_path
+  end
+
+  post '/output_pdf' do
     timetable = []
     timetable_dayofweek = ["","Mon","Tues","Wed","Thur","Fri"]
     timetable << timetable_dayofweek
-    period.times do |pt|
-      pt+=1
-      timeTable_record = Array.new(timetable_dayofweek.length)
-      timeTable_record[0] = pt
-      timetable << timeTable_record
+    records = JSON.parse(params[:records])
+    records.each_with_index do |record,idx|
+      idx+=1
+      record.unshift(idx.to_s)
+      timetable << record
     end
     file_name = File.basename(__FILE__,".rb")+".pdf"
-    f_name = "public/images/#{file_name}"
-    Prawn::Document.generate(f_name) {
+    file_path = "public/images/#{file_name}"
+    Prawn::Document.generate(file_path) {
       stroke_axis
   
       text "TimeTable"
@@ -32,7 +39,6 @@ class MainApp < Sinatra::Base
         table timetable
       }
     }
-    response.headers["Content-Disposition"] = "attachment"
-    send_file f_name
+    file_name
   end
 end
